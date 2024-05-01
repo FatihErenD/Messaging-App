65095c4106079c6f6ce02fe50895982d8cfed870
FatihErenD <fatiherendurmus@hotmail.com>
2024-05-01 14:02:14 +0300

    Login & Create

--- src/main/java/org/example/messagingapp/HelloController.java
+++ src/main/java/org/example/messagingapp/HelloController.java
@@ -2,11 +2,16 @@ package org.example.messagingapp;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HelloController {
 
@@ -26,8 +31,13 @@ public class HelloController {
     private Label yanlisHesap;
 
     @FXML
-    void onCreateAccount(MouseEvent event) {
-
+    void onCreateAccount(MouseEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("create.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1000, 600);
+        Stage stage = new Stage();
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
     }
 
     @FXML
@@ -37,6 +47,16 @@ public class HelloController {
 
     @FXML
     void onMouseEntered(MouseEvent event) {
+
+    }
+
+    @FXML
+    void OnCreate(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onAlreadyHave(MouseEvent event) {
 
     }
 
