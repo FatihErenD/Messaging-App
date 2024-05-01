e8c3a84ed265266933126d94891e535510adc3a7
FatihErenD <fatiherendurmus@hotmail.com>
2024-05-01 13:53:02 +0300

    Initial commit

---
+++ src/main/java/org/example/messagingapp/HelloController.java
@@ -0,0 +1,43 @@
+package org.example.messagingapp;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+public class HelloController {
+
+    @FXML
+    private Label hesap;
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    private PasswordField password;
+
+    @FXML
+    private TextField username;
+
+    @FXML
+    private Label yanlisHesap;
+
+    @FXML
+    void onCreateAccount(MouseEvent event) {
+
+    }
+
+    @FXML
+    void onLoginButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onMouseEntered(MouseEvent event) {
+
+    }
+
+}
